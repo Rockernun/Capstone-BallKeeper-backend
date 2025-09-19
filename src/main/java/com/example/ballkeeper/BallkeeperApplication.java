135ab25b85ce0803d67a18bbf05ad9a16d8ff595
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-09-19 17:10:35 +0900

    Initial commit

---
+++ src/main/java/com/example/ballkeeper/BallkeeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.ballkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallkeeperApplication.class, args);
+	}
+
+}
